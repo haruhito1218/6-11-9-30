7423e46c963e4898b693dd6b1e305bc3cb98e09f
haruhito1218 <haruhito151@gmail.com>
2025-06-11 09:31:41 +0900

    Add files via upload

---
+++ Kadai08.js
@@ -0,0 +1,12 @@
+let A = document.querySelector("#subutitile1");
+A.textContent="伏見稲荷大社説明";
+
+let B = document.querySelector("#subutitile2");
+B.textContent="金閣寺説明";
+
+let C = "Hello";
+alert(C);
+
+let D = document.querySelector("title");
+D.textContent="第８回課題";
+alert(D.textContent);
